9c4ff2f3636c18f6bb93e54e980e0a7c83e2e625
Youwen Wu <youwenw@gmail.com>
2025-04-21 03:07:37 -0700

    feat: poc for templating html and rendering it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use std::path::Path;
+use std::{io::Read, path::Path};
 mod world;
+use hypertext::{html_elements, maud, GlobalAttributes, Raw, Renderable};
 
 fn main() {
-    world::build_html_artifacts().unwrap();
+    // world::build_html_artifacts().unwrap();
+
+    let mut file = std::fs::File::open("./.apogee/typst-html/About.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let raw = hypertext::Raw(contents);
+
+    println!(
+        "{}",
+        maud! {
+            div #main title="Main Div" {
+                h1.important {
+                    (raw)
+                }
+            }
+        }
+        .render()
+        .as_str()
+    );
 }
