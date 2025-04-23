64e9f8d3cd4461b88e9cc3558cdb0fbafa4eeb6c
Youwen Wu <youwenw@gmail.com>
2025-04-23 01:11:07 -0700

    feat: build cli with clap

--- src/bin/cli/mod.rs
+++ src/bin/cli/mod.rs
@@ -7,21 +7,43 @@ use hypertext::Raw;
 
 use crate::templates;
 
-pub fn run() {
-    let mainpage = templates::MainPage {};
-    let aboutpage = templates::AboutPage {};
-    let blog = templates::Blog {};
+use clap::{Parser, Subcommand};
 
-    let the_world = World::new(WorkingDirs::get_dirs().unwrap());
-    let site = Site::new(the_world.get_routes(), move |slug, content| {
-        let raw_content = Raw(content);
-        let rendered = match slug.as_str() {
-            "/" => mainpage.render_page_with_content(raw_content),
-            "/about" => aboutpage.render_page_with_content(raw_content),
-            "/blog" => blog.render_page_with_content(raw_content),
-            _ => mainpage.render_page_with_content(raw_content),
-        };
-        rendered
-    });
-    the_world.build_routes(&site).unwrap();
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Compile the website. Must be ran from the root directory of the project.
+    Build,
+}
+
+pub fn run() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Build => {
+            let mainpage = templates::MainPage {};
+            let aboutpage = templates::AboutPage {};
+            let blog = templates::Blog {};
+
+            let the_world = World::new(WorkingDirs::get_dirs().unwrap());
+            let site = Site::new(the_world.get_routes(), move |slug, content| {
+                let raw_content = Raw(content);
+                let rendered = match slug.as_str() {
+                    "/" => mainpage.render_page_with_content(raw_content),
+                    "/about" => aboutpage.render_page_with_content(raw_content),
+                    "/blog" => blog.render_page_with_content(raw_content),
+                    _ => mainpage.render_page_with_content(raw_content),
+                };
+                rendered
+            });
+            the_world.build_routes(&site).unwrap();
+        }
+    }
 }
