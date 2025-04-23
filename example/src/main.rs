4dd4af1f6b62674fc73e38ab6d2b133a53d271ab
Youwen Wu <youwenw@gmail.com>
2025-04-22 22:18:31 -0700

    feat: templating closure

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,6 +3,7 @@ mod templates;
 use apogee::site::Site;
 use apogee::templating::Template;
 use apogee::world::{self, WorkingDirs, World};
+use hypertext::Raw;
 use std::path::Path;
 
 fn main() {
@@ -13,13 +14,17 @@ fn main() {
     //
     // the_world.realize_doc(&mut doc).unwrap();
     // let doc_raw = the_world.get_doc_contents(doc).unwrap();
-    // let mainpage = templates::mainpage::MainPageTemplate {};
+    let mainpage = templates::mainpage::MainPageTemplate {};
     // let final_html: String = mainpage
     //     .populate_with_generated_content(hypertext::Raw(doc_raw))
     //     .into();
     // std::fs::write("./dist/Index.html", &final_html).unwrap();
 
     let the_world = World::new(WorkingDirs::get_dirs().unwrap());
-    let site = Site::new(the_world.to_routes(), "temp".to_string());
+    let site = Site::new(the_world.to_routes(), move |slug, content| {
+        let rendered = mainpage.render_page_with_content(Raw(content));
+        println!("{}", rendered.as_str());
+        rendered
+    });
     the_world.build_site(&site).unwrap();
 }
