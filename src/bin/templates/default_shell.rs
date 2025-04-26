f35f9d572c61b93424db23924a37e7c4a3ce98ce
Youwen Wu <youwenw@gmail.com>
2025-04-26 16:21:41 -0700

    style: adjust navbar

--- src/bin/templates/default_shell.rs
+++ src/bin/templates/default_shell.rs
@@ -16,10 +16,16 @@ pub struct DefaultShell {
 impl DefaultShell {
     pub fn render_with_children(self, children: impl Renderable) -> Rendered<String> {
         let nav_items = maud! {
-            li {a class="hover:bg-overlay transition-colors" href="/luminite" {"Luminite"}}
-            li {a class="hover:bg-overlay transition-colors" href="/the-web-is-insane" {"The web is insane"}}
-            li {a class="hover:bg-overlay transition-colors" href="/math-test" {"Math"}}
             li {a class="hover:bg-overlay transition-colors" href="/impressum" {"Impressum"}}
+        };
+
+        let code = maud! {
+            li {a class="hover:bg-overlay transition-colors" href="/the-web-is-insane" {"The web is insane"}}
+            li {a class="hover:bg-overlay transition-colors" href="/luminite" {"Luminite"}}
+        };
+
+        let math = maud! {
+            li {a class="hover:bg-overlay transition-colors" href="/math-test" {"A test of Typst math rendering"}}
         };
 
         let page_width = match self.width {
@@ -32,30 +38,54 @@ impl DefaultShell {
             html lang="en" {
                 (self.head)
                 body class="font-sans antialiased leading-relaxed mx-auto max-w-[1200px]" {
-                    div class="flex gap-4 px-4 lg:px-6 lg:mt-20" {
+                    div class="flex gap-8 px-4 lg:px-6 lg:mt-20" {
                         aside class="hidden lg:block w-64 flex-none" {
-                            a class="italic text-[3em] hover:bg-rose/50 transition-colors" href="/" {"youwen wu"}
-                            nav class="space-y-4 text-2xl mt-4" {
-                                ul class="space-y-2" {
+                            a class="italic text-[3em] hover:bg-subtle/50 transition-colors select-none" href="/" {"youwen wu"}
+                            nav class="space-y-8 mt-4" {
+                                ul class="space-y-2 text-love text-2xl " {
                                     (nav_items)
+                                }
+                                div class="space-y-4" {
+                                    p class="text-3xl" {"Code"}
+                                    ul class="italic space-y-2 text-subtle text-xl" {
+                                        (code)
+                                    }
+                                }
+                                div class="space-y-4" {
+                                    p class="text-3xl" {"Math"}
+                                    ul class="italic space-y-2 text-subtle text-xl" {
+                                        (math)
+                                    }
                                 }
                             }
                         }
-                        main class=("main-content flex-1 lg:mt-2".to_owned() + page_width) {
-                            div class="lg:hidden border-b border-dashed border-muted mb-4 space-y-4 pb-4"  {
-                                p class="italic text-[3em] lg:text-start text-center" {"youwen wu"}
-                                details class="w-full" {
+                        div class="flex-1 lg:mt-2" {
+                            div class="lg:hidden border-b border-dashed border-muted mb-8 pb-8 w-full"  {
+                                p class="text-center italic text-[3em] hover:text-subtle mt-8 select-none" {a href="/" {"youwen wu"}}
+                                details class="w-full mt-8" {
                                     summary class="text-center smallcaps text-xl cursor-pointer" {
                                         "menu"
                                     }
-                                    nav class="space-y-3 text-2xl" {
-                                        ul class="space-y-4 text-xl" {
+                                    nav class="space-y-8 text-2xl mt-3" {
+                                        ul class="space-y-3 text-2xl text-love" {
                                             (nav_items)
                                         }
+                                        div class="space-y-3" {
+                                            p class="text-2xl" {"Code"}
+                                            ul class="italic space-y-2 text-subtle text-xl" {
+                                                (code)
+                                            }
+                                            p class="text-2xl" {"Math"}
+                                            ul class="italic space-y-2 text-subtle text-xl" {
+                                                (math)
+                                            }
+                                }
                                     }
                                 }
                             }
-                            (children)
+                            main class=("main-content".to_owned() + page_width) {
+                                (children)
+                            }
                             footer class="border-t border-solid border-muted mb-4 flex justify-between items-center gap-4 text-lg text-muted py-1 mt-10" {
                                 p class="smallcaps" {"© 2025 Youwen Wu. Proudly generated by " a class="text-rose" style="text-decoration: none !important;" href="https://github.com/youwen5/luminite" {"luminite"}"."}
                             }
