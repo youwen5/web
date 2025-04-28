bcdaf37cbc12a6c00d4f1213a165db18b31c864b
Youwen Wu <youwenw@gmail.com>
2025-04-27 18:51:14 -0700

    style: bold light mode headers

--- src/bin/templates/main_page.rs
+++ src/bin/templates/main_page.rs
@@ -24,7 +24,7 @@ impl Template for MainPage {
             width: super::default_shell::PageWidth::Wide,
         }
         .render_with_children(maud! {
-            div id="typst-injected" class="prose-xl lg:prose-2xl mt-2 prose-headings:all-smallcaps prose-headings:text-center prose-h1:text-4xl lg:prose-headings:text-start lg:prose-h1:text-[2.5rem]" {
+            div id="typst-injected" class="prose-xl lg:prose-2xl mt-2 prose-headings:all-smallcaps prose-headings:text-center prose-h1:text-4xl lg:prose-headings:text-start lg:prose-h1:text-[2.5rem] prose-headings:font-bold prose-headings:dark:font-normal" {
                 @if let Some(title) = title {
                      h1 {
                          (title)
