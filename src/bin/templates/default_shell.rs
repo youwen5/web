462fe54d086c4f374794c64c8cfb223389809a79
Youwen Wu <youwenw@gmail.com>
2025-04-28 15:28:41 -0700

    chore: check in rustfmt.toml with style_edition=2024

--- src/bin/templates/default_shell.rs
+++ src/bin/templates/default_shell.rs
@@ -1,4 +1,4 @@
-use hypertext::{html_elements, maud, GlobalAttributes, Renderable, Rendered};
+use hypertext::{GlobalAttributes, Renderable, Rendered, html_elements, maud};
 
 use super::components::Head;
 
